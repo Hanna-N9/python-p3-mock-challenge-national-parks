4431456727ff64877cb38dd87277460ee6b9f336
Hanna Negash <hnegash.n@gmail.com>
2023-12-14 14:58:29 -0800

    Complete National Parks (Object Relationships) Mock Challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,40 +1,121 @@
 class NationalPark:
+    all = []
 
     def __init__(self, name):
         self.name = name
+        NationalPark.all.append(self)
         
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and not hasattr(self, "name"):
+            self._name = name
+                    
+    #Returns a list of all trips at a particular national park, Trips must be of type Trip    
     def trips(self):
-        pass
+        return [trip for trip in Trip.all if trip.national_park == self]
     
+    #Returns a unique list of all visitors a particular national park has welcomed, Visitors must be of type Visitor
     def visitors(self):
-        pass
-    
+        return list({trip.visitor for trip in self.trips()})
+
+    #Returns the total number of times a park has been visited, Returns 0 if the park has no visits
     def total_visits(self):
-        pass
+        return len(self.trips())
     
+    #Returns the Visitor instance that has visited that park the most, Returns None if the park has no visitors
     def best_visitor(self):
-        pass
+        return max(set(self.visitors()), key = self.visitors().count)
+
+
+
+
 
 
 class Trip:
+    all = []
     
     def __init__(self, visitor, national_park, start_date, end_date):
         self.visitor = visitor
         self.national_park = national_park
         self.start_date = start_date
         self.end_date = end_date
+        Trip.all.append(self)
+
+    @property
+    def start_date(self):
+        return self._start_date
+
+    @start_date.setter
+    def start_date(self, start_date):
+        if isinstance(start_date, str) and len(start_date) >= 7:
+            self._start_date = start_date
+            
+    @property
+    def end_date(self):
+        return self._end_date
+
+    @end_date.setter
+    def end_date(self, end_date):
+        if isinstance(end_date, str) and len(end_date) >= 7:
+            self._end_date = end_date
+            
+    @property
+    def visitor(self):
+        return self._visitor
+
+    @visitor.setter
+    def visitor(self, visitor):
+        if isinstance(visitor, Visitor):
+            self._visitor = visitor
+
+    @property
+    def national_park(self):
+        return self._national_park
+
+    @national_park.setter
+    def national_park(self, national_park):
+        if isinstance(national_park, NationalPark):
+            self._national_park = national_park
+
+
+
+
+
 
 
 class Visitor:
+    all = []
 
     def __init__(self, name):
         self.name = name
-        
+        Visitor.all.append(self)
+           
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 15:
+            self._name = name
+    
+    #Returns a list of all trips for that visitor, Trips must be of type Trip       
     def trips(self):
-        pass
-    
+        return [trip for trip in Trip.all if trip.visitor == self]
+
+    #Returns a unique list of all parks that visitor has visited, Parks must be of type NationalPark
     def national_parks(self):
-        pass
+        return list({trip.national_park for trip in self.trips()})
     
+    #Returns the total number of times a visitor visited the park passed in as argument, Returns 0 if the visitor has never visited the park
     def total_visits_at_park(self, park):
-        pass
+        if park.visitors():
+            return len([trip for trip in self.trips() if trip.national_park == park])
+        return 0 
+    
+             
+    
